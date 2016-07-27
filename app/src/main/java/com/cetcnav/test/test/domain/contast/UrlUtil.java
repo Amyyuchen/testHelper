43d293b837af9a81276664b8a492a3c5c85804a3
Amyyuchen <shixin666@qq.com>
2016-07-27 11:14:37 +0800

    Signed-off-by: Amyyuchen <shixin666@qq.com>

--- app/src/main/java/com/cetcnav/test/test/domain/contast/UrlUtil.java
+++
@@ -1,9 +0,0 @@
-package com.cetcnav.test.test.domain.contast;
-
-/**
- * Created by 邢皓翔 on 2016/7/14.
- */
-public class UrlUtil {
-    public static final String BASE_URL = "http://apis.baidu.com";
-    public static final String API_KEY = "7239b1d3a3d624a51fd102eab676e859";
-}
